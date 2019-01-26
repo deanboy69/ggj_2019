6c9ac6b710528b48de36b37f41d24e1c755506ad
rikzander <rikzander94@gmail.com>
2019-01-26 00:42:29 -0500

    player movement

--- GGJ 2019/scripts/player.gd
+++ GGJ 2019/scripts/player.gd
@@ -1,15 +1,27 @@
 extends KinematicBody2D
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
+var pos = Vector2()
 
 func _ready():
-	# Called when the node is added to the scene for the first time.
-	# Initialization here
+	#var pos = get_global_position()
 	pass
-
+	
+	
+func _physics_process(delta):
+	pos = get_global_position()
+	player_input()
+	set_global_position(pos)
 #func _process(delta):
 #	# Called every frame. Delta is time since last frame.
 #	# Update game logic here.
 #	pass
+func player_input():
+	if Input.is_action_pressed("ui_up"):
+		pos += Vector2(0,-10)
+	if Input.is_action_pressed("ui_down"):
+		pos += Vector2(0,10)
+	if Input.is_action_pressed("ui_left"):
+		pos += Vector2(-10,0)
+	if Input.is_action_pressed("ui_right"):
+		pos += Vector2(10,0)
+		
