7939e33dc60353c213123f6e630cfeb0829cbf17
rikzander <rikzander94@gmail.com>
2019-01-26 01:44:55 -0500

    player movemement

--- GGJ 2019/scripts/player.gd
+++ GGJ 2019/scripts/player.gd
@@ -1,5 +1,5 @@
 extends KinematicBody2D
-
+var speed = 5
 var pos = Vector2()
 
 func _ready():
@@ -10,18 +10,19 @@ func _ready():
 func _physics_process(delta):
 	pos = get_global_position()
 	player_input()
-	set_global_position(pos)
+	#set_global_position(pos)
 #func _process(delta):
 #	# Called every frame. Delta is time since last frame.
 #	# Update game logic here.
 #	pass
 func player_input():
 	if Input.is_action_pressed("ui_up"):
-		pos += Vector2(0,-10)
+		position += Vector2(0,-speed)
 	if Input.is_action_pressed("ui_down"):
-		pos += Vector2(0,10)
+		position += Vector2(0,speed)
 	if Input.is_action_pressed("ui_left"):
-		pos += Vector2(-10,0)
+		position += Vector2(-speed,0)
 	if Input.is_action_pressed("ui_right"):
-		pos += Vector2(10,0)
-		
+		position += Vector2(speed,0)
+	if Input.is_action_pressed("punch"):
+		pass
