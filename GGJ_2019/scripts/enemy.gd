09036f270983837233add2e91e93c726b584ff42
rikzander <rikzander94@gmail.com>
2019-01-26 11:09:50 -0500

    jesus christ

--- GGJ_2019/scripts/enemy.gd
+++ GGJ_2019/scripts/enemy.gd
@@ -3,7 +3,9 @@ extends KinematicBody2D
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
-onready var dict1 = {'1':['hobo1'
+onready var hobo1 = {'health':100,'punch':10,'kick':15,'skin':1}
+onready var hobo2 = {'health':100,'punch':10,'kick':15,'skin':2}
+onready var boss1 = {'health':150,'punch':20,'kick':30,'skin':1}
 
 func _ready():
 	# Called when the node is added to the scene for the first time.
